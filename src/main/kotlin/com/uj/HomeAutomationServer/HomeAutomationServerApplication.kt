071b19153608f662e249a579a02b2003fac6f0a3
Michael Brooke <michaelbrooke93@gmail.com>
2017-08-09 19:48:56 +0200

    Initial commit

---
+++ src/main/kotlin/com/uj/HomeAutomationServer/HomeAutomationServerApplication.kt
@@ -0,0 +1,11 @@
+package com.uj.HomeAutomationServer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HomeAutomationServerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(HomeAutomationServerApplication::class.java, *args)
+}
